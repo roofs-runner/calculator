84f1cac80fde1d13ecaf70b95c2c504b9f57ba2e
Sergiy Svitlookyi <bourneind@gmail.com>
2020-07-12 09:25:26 +0300

    Updates

---
+++ precache-manifest.09327c11f764333a4e169ff44ada4cd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533c4b44cbd29d50f9721ce99bad936f",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "8ff43ab3ee3520667923",
+    "url": "/calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3fe04939e07c9964366a",
+    "url": "/calculator/static/js/2.809a5bc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculator/static/js/2.809a5bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff43ab3ee3520667923",
+    "url": "/calculator/static/js/main.2bb8d858.chunk.js"
+  },
+  {
+    "revision": "ee4a148f714da062db4f",
+    "url": "/calculator/static/js/runtime-main.f0d0de70.js"
+  }
+]);
